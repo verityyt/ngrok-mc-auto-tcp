24904a0caa0832d8b961ae97475fca09ccda50b7
Verity <joshua-schmidt@online.de>
2021-03-05 00:55:10 +0100

    :sparkles: Add ServerManager (startup mc server)

---
+++ src/main/kotlin/minecraft/ServerManager.kt
@@ -0,0 +1,56 @@
+package minecraft
+
+import sun.security.krb5.internal.ccache.CCacheOutputStream
+import utils.CConsole
+import utils.CConsoleColor
+import java.io.File
+import kotlin.system.exitProcess
+
+object ServerManager {
+
+    fun start(path: String, ram: Int, jarName: String = "server") {
+
+        CConsole.println(
+            "[INFO] Starting up server in '/${File(path).name}' with '${ram}MB' max. ram...",
+            CConsoleColor.Cyan
+        )
+
+        if (NgrokMcAutoTcp.debug) {
+            CConsole.println("[DEBUG] Creating batch file...", CConsoleColor.Red)
+        }
+
+        val batch = File("$path\\cur.bat")
+        batch.writeText("java -Xmx${ram}M -Xms${ram}M -jar $jarName.jar nogui\npause")
+
+        if (batch.readText() != "") {
+            if (NgrokMcAutoTcp.debug) {
+                CConsole.println("[DEBUG] Batch file successfully created!", CConsoleColor.Red)
+            }
+        } else {
+            CConsole.println("----------------------------------", CConsoleColor.Red)
+            CConsole.println("Batch file creation failed!", CConsoleColor.Red)
+            CConsole.println("----------------------------------", CConsoleColor.Red)
+            exitProcess(-1)
+        }
+
+        Thread.sleep(1000)
+
+        if (NgrokMcAutoTcp.debug) {
+            CConsole.println("[DEBUG] Running batch file...", CConsoleColor.Red)
+        }
+
+        val proc =
+            Runtime.getRuntime().exec("cmd /c start ${batch.name}", null, File(path))
+
+        if (proc.isAlive) {
+            CConsole.println("[INFO] Server successfully started!", CConsoleColor.Cyan)
+        } else {
+            CConsole.println("----------------------------------", CConsoleColor.Red)
+            CConsole.println("Server startup failed!", CConsoleColor.Red)
+            CConsole.println("----------------------------------", CConsoleColor.Red)
+            exitProcess(-1)
+        }
+
+    }
+
+}
